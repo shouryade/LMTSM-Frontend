6035fd57035c868ca447389399ec4bd148458ca8
shouryade <shourya.de12@gmail.com>
2023-07-17 11:42:17 +0530

    modified print ready
    Signed-off-by: shouryade <shourya.de12@gmail.com>

--- src/components/travelComponents/PrintReadyComponent.jsx
+++ src/components/travelComponents/PrintReadyComponent.jsx
@@ -119,7 +119,11 @@ const PrintReadyComponent = () => {
               </div>
 
               {dutySlips.map((dutySlip, index) => (
-                <div className="flex flex-wrap border border-gray-500 p-4 mb-4 text-sm">
+                <div
+                  key={index}
+                  className="flex flex-wrap border border-gray-500 p-4 mb-4 text-sm"
+                >
+                  {" "}
                   <div className="border border-gray-500 p-2 mx-1">
                     <strong>Booking For : </strong> {dutySlip.booking_name},
                     <span className="text-blue-500"> {dutySlip.phone}</span>, (
@@ -136,7 +140,6 @@ const PrintReadyComponent = () => {
                       {" "}
                       {dutySlip.vehicle_number}
                     </span>
-                    {/* {dutySlip.phone_of_driver} */}
                   </div>
                   <div className="border border-gray-500 p-2 mx-1">
                     <strong>Trip Details : </strong>{" "}
